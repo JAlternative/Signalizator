d6d111375097e3dcd50c01c9c45acad7e1716d31
JAlternative <s.khoroshkov@gmail.com>
2026-05-26 23:56:25 +0300

    controller

--- app-backend/src/main/java/backend/controller/MoexDebugController.java
+++ app-backend/src/main/java/backend/controller/MoexDebugController.java
@@ -1,14 +1,12 @@
 package backend.controller;
 
 import backend.integration.moex.dto.MoexMarketDataSnapshot;
+import backend.persistence.entity.PricePoint;
 import backend.service.MoexMarketDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -33,6 +31,14 @@ public class MoexDebugController {
     @GetMapping("/instruments/{id}/snapshot")
     public ResponseEntity<MoexMarketDataSnapshot> getMarketDataSnapshotByInstrumentId(@PathVariable Long id) {
         return moexMarketDataService.getMarketDataSnapshotByInstrumentId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/instruments/{id}/collect")
+    public ResponseEntity<Long> collectAndSaveSnapshot(@PathVariable Long id) {
+        return moexMarketDataService.collectAndSaveSnapshot(id)
+                .map(PricePoint::getId)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
